01492f1cab06a1fb3597e59b8e71d99ac24a7508
Faiz Mohammad <faizmohammadramdel@gmail.com>
2023-11-29 20:30:24 +0430

    template configured

---
+++ resources/views/frontend/gallery/gallery.blade.php
@@ -0,0 +1,75 @@
+@extends('frontend.layout.master')
+
+@section('contents')
+
+
+    <!-- Gallery Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto pb-4 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                <p class="section-title bg-white text-center text-primary px-3">Gallery</p>
+                <h1 class="mb-5">Explore Our Dairy Farm Gallery</h1>
+            </div>
+            <div class="row g-0">
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="row g-0">
+                        <div class="col-12">
+                            <a class="d-block" href="img/gallery-5.jpg')}}" data-lightbox="gallery">
+                                <img class="img-fluid" src="{{asset('frontend/img/gallery-5.jpg')}}" alt="">
+                            </a>
+                        </div>
+                        <div class="col-12">
+                            <a class="d-block" href="img/gallery-1.jpg')}}" data-lightbox="gallery">
+                                <img class="img-fluid" src="{{asset('frontend/img/gallery-1.jpg')}}" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="row g-0">
+                        <div class="col-12">
+                            <a class="d-block" href="img/gallery-2.jpg')}}" data-lightbox="gallery">
+                                <img class="img-fluid" src="{{asset('frontend/img/gallery-2.jpg')}}" alt="">
+                            </a>
+                        </div>
+                        <div class="col-12">
+                            <a class="d-block" href="img/gallery-6.jpg')}}" data-lightbox="gallery">
+                                <img class="img-fluid" src="{{asset('frontend/img/gallery-6.jpg')}}" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="row g-0">
+                        <div class="col-12">
+                            <a class="d-block" href="img/gallery-7.jpg')}}" data-lightbox="gallery">
+                                <img class="img-fluid" src="{{asset('frontend/img/gallery-7.jpg')}}" alt="">
+                            </a>
+                        </div>
+                        <div class="col-12">
+                            <a class="d-block" href="img/gallery-3.jpg')}}" data-lightbox="gallery">
+                                <img class="img-fluid" src="{{asset('frontend/img/gallery-3.jpg')}}" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="row g-0">
+                        <div class="col-12">
+                            <a class="d-block" href="img/gallery-4.jpg')}}" data-lightbox="gallery">
+                                <img class="img-fluid" src="{{asset('frontend/img/gallery-4.jpg')}}" alt="">
+                            </a>
+                        </div>
+                        <div class="col-12">
+                            <a class="d-block" href="img/gallery-8.jpg')}}" data-lightbox="gallery">
+                                <img class="img-fluid" src="{{asset('frontend/img/gallery-8.jpg')}}" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Gallery End -->
+
+@endsection
---
+++ resources/views/frontend/services/services.blade.php
@@ -0,0 +1,116 @@
+@extends('frontend.layout.master')
+
+@section('contents')
+    <!-- Service Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto pb-4 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                <p class="section-title bg-white text-center text-primary px-3">Our Services</p>
+                <h1 class="mb-5">Services That We Offer For Entrepreneurs</h1>
+            </div>
+            <div class="row gy-5 gx-4">
+                <div class="col-lg-4 col-md-6 pt-5 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item d-flex h-100">
+                        <div class="service-img">
+                            <img class="img-fluid" src="img/service-1.jpg" alt="">
+                        </div>
+                        <div class="service-text p-5 pt-0">
+                            <div class="service-icon">
+                                <img class="img-fluid rounded-circle" src="img/service-1.jpg" alt="">
+                            </div>
+                            <h5 class="mb-3">Best Animal Selection</h4>
+                            <p class="mb-4">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.</p>
+                            <a class="btn btn-square rounded-circle" href=""><i class="bi bi-chevron-double-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 pt-5 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="service-item d-flex h-100">
+                        <div class="service-img">
+                            <img class="img-fluid" src="img/service-2.jpg" alt="">
+                        </div>
+                        <div class="service-text p-5 pt-0">
+                            <div class="service-icon">
+                                <img class="img-fluid rounded-circle" src="img/service-2.jpg" alt="">
+                            </div>
+                            <h5 class="mb-3">Breeding & Veterinary</h5>
+                            <p class="mb-4">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.</p>
+                            <a class="btn btn-square rounded-circle" href=""><i class="bi bi-chevron-double-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 pt-5 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="service-item d-flex h-100">
+                        <div class="service-img">
+                            <img class="img-fluid" src="img/service-3.jpg" alt="">
+                        </div>
+                        <div class="service-text p-5 pt-0">
+                            <div class="service-icon">
+                                <img class="img-fluid rounded-circle" src="img/service-3.jpg" alt="">
+                            </div>
+                            <h5 class="mb-3">Care & Milking</h5>
+                            <p class="mb-4">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.</p>
+                            <a class="btn btn-square rounded-circle" href=""><i class="bi bi-chevron-double-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Service End -->
+
+
+    <!-- Features Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5 align-items-center">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <p class="section-title bg-white text-start text-primary pe-3">Why Us!</p>
+                    <h1 class="mb-4">Few Reasons Why People Choosing Us!</h1>
+                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>Justo magna erat amet</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>Aliqu diam amet diam et eos</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>Clita erat ipsum et lorem et sit</p>
+                    <a class="btn btn-secondary rounded-pill py-3 px-5 mt-3" href="">Explore More</a>
+                </div>
+                <div class="col-lg-6">
+                    <div class="rounded overflow-hidden">
+                        <div class="row g-0">
+                            <div class="col-sm-6 wow fadeIn" data-wow-delay="0.1s">
+                                <div class="text-center bg-primary py-5 px-4">
+                                    <img class="img-fluid mb-4" src="img/experience.png" alt="">
+                                    <h1 class="display-6 text-white" data-toggle="counter-up">25</h1>
+                                    <span class="fs-5 fw-semi-bold text-secondary">Years Experience</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 wow fadeIn" data-wow-delay="0.3s">
+                                <div class="text-center bg-secondary py-5 px-4">
+                                    <img class="img-fluid mb-4" src="img/award.png" alt="">
+                                    <h1 class="display-6" data-toggle="counter-up">183</h1>
+                                    <span class="fs-5 fw-semi-bold text-primary">Award Winning</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 wow fadeIn" data-wow-delay="0.5s">
+                                <div class="text-center bg-secondary py-5 px-4">
+                                    <img class="img-fluid mb-4" src="img/animal.png" alt="">
+                                    <h1 class="display-6" data-toggle="counter-up">2619</h1>
+                                    <span class="fs-5 fw-semi-bold text-primary">Total Animals</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 wow fadeIn" data-wow-delay="0.7s">
+                                <div class="text-center bg-primary py-5 px-4">
+                                    <img class="img-fluid mb-4" src="img/client.png" alt="">
+                                    <h1 class="display-6 text-white" data-toggle="counter-up">51940</h1>
+                                    <span class="fs-5 fw-semi-bold text-secondary">Happy Clients</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Features End -->
+
+
+@endsection()
